a5901627378ab87eb331685db3cf93ea1e2b070a
penrodlol <penrod.christian7984@gmail.com>
2023-01-08 18:44:32 -0500

    ♻️ Clean up posts script

--- scripts/posts.mjs
+++ scripts/posts.mjs
@@ -2,28 +2,21 @@ import { XMLParser } from "fast-xml-parser";
 import { readFileSync, writeFileSync } from "fs";
 import fetch from "node-fetch";
 
-const parser = new XMLParser();
+const payload = await fetch("https://christianpenrod.com/rss.xml");
 
-(async () => {
-  const response = await fetch("https://christianpenrod.com/rss.xml");
-  const xml = await response.text();
-  const data = parser.parse(xml);
-  const posts = data.rss.channel.item
-    .sort((a, b) => {
-      const aDate = new Date(a.pubDate).valueOf();
-      const bDate = new Date(b.pubDate).valueOf();
-      return bDate - aDate;
-    })
-    .slice(0, 3)
-    .map((post) => ({ title: post.title, link: post.link }));
+const rawRSS = await payload.text();
+const parsedRSS = new XMLParser().parse(rawRSS).rss.channel.item;
 
-  const readme = readFileSync("README.md", "utf8");
-  const newReadme = readme.replace(
-    /<!-- POSTS_START -->[\s\S]*<!-- POSTS_END -->/,
-    `<!-- POSTS_START -->\n${posts
-      .map((post) => `➩ [${post.title}](${post.link})<br/>`)
-      .join("\n")}\n<!-- POSTS_END -->`
-  );
+const posts = parsedRSS
+  .sort((a, b) => new Date(b.pubDate).valueOf() - new Date(a.pubDate).valueOf())
+  .slice(0, 3);
 
-  writeFileSync("README.md", newReadme);
-})();
+const readme = readFileSync("README.md", "utf8");
+const nextReadme = readme.replace(
+  /<!-- POSTS_START -->[\s\S]*<!-- POSTS_END -->/,
+  `<!-- POSTS_START -->\n${posts
+    .map((post) => `➩ [${post.title}](${post.link})<br/>`)
+    .join("\n")}\n<!-- POSTS_END -->`
+);
+
+writeFileSync("README.md", nextReadme);
