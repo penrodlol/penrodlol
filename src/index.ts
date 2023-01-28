d684b23f57e9233ad3b3ce7191a3991d8f0c575f
penrodlol <penrod.christian7984@gmail.com>
2023-01-28 17:52:43 -0500

    💄 Reformat recent posts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import dayjs from 'dayjs';
 import { renderFile } from 'ejs';
 import { writeFileSync } from 'fs';
 import { join } from 'path';
@@ -11,13 +10,10 @@ const posts = z
     z.object({
       title: z.string(),
       link: z.string(),
-      pubDate: z.string().transform((date) => ({
-        time: new Date(date).valueOf(),
-        formatted: dayjs(date).format('MMMM D, YYYY'),
-      })),
+      pubDate: z.string().transform((date) => new Date(date).valueOf()),
     }),
   )
-  .refine((r) => r.sort((a, b) => b.pubDate.time - a.pubDate.time))
+  .refine((r) => r.sort((a, b) => b.pubDate - a.pubDate).slice(0, 5))
   .safeParse(rss.items);
 
 if (posts.success) {
