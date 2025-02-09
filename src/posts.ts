3e046986dbd6ba0c041cb27eff9c34edb266dfc0
penrodlol <christian@penrod.contact>
2025-02-08 23:14:26 -0500

    ⚰️ Not needed

--- src/posts.ts
+++ src/posts.ts
@@ -2,7 +2,7 @@ import Parser from 'rss-parser';
 import z from 'zod';
 
 export default async function () {
-  const rss = await new Parser().parseURL('christianpenrod.com/rss.xml');
+  const rss = await new Parser().parseURL('https://christianpenrod.com/rss.xml');
   if (!rss.items) throw new Error('No items found in RSS feed');
 
   const posts = z.array(z.object({ title: z.string(), link: z.string().url() })).safeParse(rss.items);
